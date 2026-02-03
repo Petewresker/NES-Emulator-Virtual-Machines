dbe4ff94afe94652ea889da1b8ab17def94bc0a0
Petewresker <minecopper.55@gmail.com>
2026-02-03 12:16:54 +0700

    Backup version 1 of the program

---
+++ backup/version1/src/Bus.cpp
@@ -0,0 +1,17 @@
+#include "../include/Bus.h"
+
+Bus::Bus() { 
+    ram.fill(0x00); 
+    cpu.ConnectBus(this);
+}
+
+void Bus::write(uint16_t addr, uint8_t data) {
+    if (addr >= 0x0000 && addr <= 0xFFFF) ram[addr] = data;
+}
+
+uint8_t Bus::read(uint16_t addr, bool bReadOnly) {
+    if (addr >= 0x0000 && addr <= 0xFFFF) {
+        return ram[addr];
+    }
+    return 0x00;
+}
